3a1f7acd6c4136674feb6fdd85856e9c30595e6f
유키 <yukiho5048@naver.com>
2017-12-02 00:13:16 +0900

    init

---
+++ OriginDLPatcher/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OriginDLPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.Chocolate;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Patch Origin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.linkLabel1.Location = new System.Drawing.Point(93, 68);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(107, 15);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Yuki (@YukihoAA)";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(40, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Made By";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(212, 96);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Origin Download Booster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OriginDLPatcher/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace OriginDLPatcher
+{
+    public partial class Form1 : Form
+    {
+        bool isPatched = false;
+        string IniFile = "";
+        
+        void InitialCheck()
+        {
+            string ClientPath = "";
+            ClientPath = (string)Microsoft.Win32.Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Origin", "ClientPath", "");
+            if (ClientPath.Equals(""))
+                ClientPath = (string)Microsoft.Win32.Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Origin", "ClientPath", "");
+            if (ClientPath.Equals(""))
+            {
+                System.Windows.Forms.MessageBox.Show("Origin not detected. Please reinstall origin");
+                Close();
+            }
+            ClientPath = ClientPath.Substring(0, ClientPath.LastIndexOf('\\'));
+            IniFile = ClientPath + "\\EACore.ini";
+
+            Process[] processList = Process.GetProcessesByName("origin");
+            if(processList.Length > 0)
+            {
+                processList = Process.GetProcessesByName("origin");
+                System.Windows.Forms.MessageBox.Show("Please turn off origin at taskbar.");
+                Close();
+            }
+        }
+
+        bool PatchCheck()
+        {
+            bool isEnv = false, isCdn = false;
+            string[] lines = System.IO.File.ReadAllLines(IniFile);
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("EnvironmentName=production"))
+                    isEnv = true;
+
+                if (line.Contains("CdnOverride=akamai"))
+                    isCdn = true;
+            }
+
+            isPatched = isCdn && isEnv;
+            if (isPatched)
+                this.button1.Text = "Unpatch Origin";
+            else
+                this.button1.Text = "Patch Origin";
+            return isPatched;
+        }
+
+        public Form1()
+        {
+            System.Drawing.Text.PrivateFontCollection privateFonts =
+                      new System.Drawing.Text.PrivateFontCollection();
+            var fontBytes = Properties.Resources._210R_TTF;
+            var fontData = Marshal.AllocCoTaskMem(fontBytes.Length);
+            Marshal.Copy(fontBytes, 0, fontData, fontBytes.Length);
+            privateFonts.AddMemoryFont(fontData, fontBytes.Length);
+            Marshal.FreeCoTaskMem(fontData);
+
+            InitialCheck();
+            InitializeComponent();
+            PatchCheck();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isPatched)
+            {
+                System.IO.File.WriteAllText(IniFile, "");
+
+                if (!PatchCheck()) {
+                    System.Windows.Forms.MessageBox.Show("Unpatch Complete!", "Created by @YukihoAA");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Cannot Patch Files");
+                    Close();
+                }
+            }
+            else
+            {
+                System.IO.File.WriteAllText(IniFile, "[connection]\r\n" +
+                "    EnvironmentName=production\r\n\r\n" +
+                "[Feature]\r\n" +
+                "    CdnOverride=akamai");
+
+                if (PatchCheck())
+                {
+                    System.Windows.Forms.MessageBox.Show("Patch Complete!", "Created by @YukihoAA");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Cannot Patch Files");
+                    Close();
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("explorer", "https://github.com/YukihoAA");
+        }
+    }
+}
